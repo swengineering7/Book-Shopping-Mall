48bb8c2184f1542a69ce84c63c7ab9d9be4d8476
xx10222 <yjw4960@gmail.com>
2021-05-26 12:00:22 +0900

    app.js 수정 - 로그인 및 회원가입 화면 연결

--- login_session/app.js
+++ login_session/app.js
@@ -3,6 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var flash = require('connect-flash');
+var passport=require('passport');
+var session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
